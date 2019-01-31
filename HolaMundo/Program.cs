f6135c67ddf0b86f02cd4b825f2d4d11e1ffccaf
Carlos Castaneda <castr@MacBook.local>
2019-01-31 11:45:26 -0600

    Hay te va todo agarrate

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HolaMundo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre="";
+            Console.Write("Dame tu nombre ");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine($"Hello World! {nombre}");
+            
+        }
+    }
+}
---
+++ PagaTrabajador/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PagaTrabajador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String nombre;
+            int horas;
+            float paga, tasa=0.1f;
+            float pagabruta, impuesto, paganeta;
+
+            // Console.Clear();
+            Console.WriteLine("Programa que Calcula la paga de un Trabajador");
+
+            // Entrada
+            Console.Write("Nombre del trabajador  : "); nombre=Console.ReadLine();
+            Console.Write("Horas Trabajadas       : "); horas=int.Parse(Console.ReadLine());
+            Console.Write("Paga por hora          : "); paga=int.Parse(Console.ReadLine());
+
+            // Proceso
+            pagabruta = horas * paga;
+            impuesto  = pagabruta * tasa;
+            paganeta  = pagabruta - impuesto;
+
+            // Salida
+            Console.WriteLine($"El trbajador {nombre}");
+            Console.WriteLine($"Trabajo {horas} horas, se le pago {paga} pesos por hora");
+            Console.WriteLine($"Se tiene un impuesto de {impuesto} ");
+            Console.WriteLine($"Se tiene una pagabruta de {pagabruta} ");
+            Console.WriteLine($"Se tiene una paganeta de {paganeta} ");
+        }
+    }
+}
